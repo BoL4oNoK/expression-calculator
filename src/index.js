d814efccfef42007ff07dfc2ebe15ac85be61eca
Maksim Antonov <33826709+BoL4oNoK@users.noreply.github.com>
2020-03-03 14:42:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,69 @@ function eval() {
     return;
 }
 
+function calculator(expr) {
+    let arrExpr = expr.split(' ');
+    let i = 0;
+    while( i < arrExpr.length - 1) {
+        i++;
+        if (arrExpr[i] == "*") {
+            arrExpr[i] = Number(arrExpr[i-1]) * Number(arrExpr[i+1]);
+            arrExpr.splice(i-1, 1);
+            arrExpr.splice(i, 1);
+            i = i - 1;
+        }
+
+        if (arrExpr[i] == "/") {
+            if (arrExpr[i+1] == 0) throw new TypeError('TypeError: Division by zero.');
+            arrExpr[i] = Number(arrExpr[i-1]) / Number(arrExpr[i+1]);
+            arrExpr.splice(i-1, 1);
+            arrExpr.splice(i, 1);
+            i = i - 1;
+        }
+    }
+
+    i = 0;
+    while( i < arrExpr.length - 1) {
+        i++;
+        if (arrExpr[i] == "+") {
+            arrExpr[i] = Number(arrExpr[i-1]) + Number(arrExpr[i+1]);
+            arrExpr.splice(i-1, 1);
+            arrExpr.splice(i, 1);
+            i = i - 1;
+        }
+
+        if (arrExpr[i] == "-") {
+            arrExpr[i] = Number(arrExpr[i-1]) - Number(arrExpr[i+1]);
+            arrExpr.splice(i-1, 1);
+            arrExpr.splice(i, 1);
+            i = i - 1;
+        }
+    }
+    return Number(arrExpr[0]);
+}
+
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.replace(/\s/g, '').replace(/(\*|\/|\+|\-)/g, ' $& ');
+
+    opened_br = ((br = expr.match(/\(/g)) != null) ? br.length : 0;
+    closed_br = ((br = expr.match(/\)/g)) != null) ? br.length : 0;
+    if (opened_br !== closed_br) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+
+    let subCalc;
+    while (opened_br > 0){
+        if ((subCalc = expr.match(/(\([0-9\+\/\*\-. ]+\))/g)) !== null ) {
+                let str = subCalc[0].replace('(','').replace(')','');
+                expr = expr.replace(subCalc[0], calculator(str));
+        }
+        
+        opened_br -= 1;
+    }
+
+    let res = calculator(expr);
+  
+    return res;
 }
 
 module.exports = {
